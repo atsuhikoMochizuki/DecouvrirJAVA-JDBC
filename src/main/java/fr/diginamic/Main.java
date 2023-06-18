4086e5a9a1834cc686234bef782b67639c1a9578
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-06-18 15:37:45 +0200

    tp3 terminé

--- src/main/java/fr/diginamic/Main.java
+++ src/main/java/fr/diginamic/Main.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class Main {
 
@@ -19,63 +22,55 @@ public class Main {
         Utils.clearConsole();
         Params.welcomePrompt();
 
-//        Utils.msgTitle("TP1 : Se connecter à une base de données locale");
-//
-//        final String URL = "jdbc:mysql://localhost:3306/DecouvrirJDBC_Compta";
-//        final String USER = "root";
-//        final String PWD = "root";
-//
-//        Utils.msgInfo("Connection à la base de données");
-//        try(Connection db1 = DriverManager.getConnection(URL,USER,PWD))
-//        {
-//        } catch (SQLTimeoutException timeOutError){
-//            LOG.error(timeOutError.getMessage());
-//            throw new RuntimeException(timeOutError);
-//        } catch (SQLException accessError) {
-//            LOG.error(accessError.getMessage());
-//            throw new RuntimeException(accessError);
-//        } finally{
-//            Utils.msgInfo("Connection à la base de données OK");
-//        }
-//
-//        Utils.msgTitle("TP2 : Créer un fichier de configuration pour l'accès aux données");
-//
-//        Utils.msgInfo("Import des propriétés de connection dans le fichiers properties");
-//        ResourceBundle project = ResourceBundle.getBundle("project");
-//        String URL_BY_PROP = project.getString("database.url");
-//        String USER_BY_PROP = project.getString("database.user");
-//        String PWD_BY_PROP = project.getString("database.pwd");
-//        Utils.msgInfo("Import des ressources OK");
-//
-//        Utils.msgInfo("Connection à la base de données distante (Clever-cloud)");
-//
-//        //On utilise ici le bloc finally
-//        try
-//        {
-//            db = DriverManager.getConnection(
-//                    URL_BY_PROP,
-//                    USER_BY_PROP,
-//                    PWD_BY_PROP);
-//            Utils.msgInfo("Connection à la base de données OK");
-//        }
-//        catch(SQLTimeoutException timeOutError){
-//            LOG.error(timeOutError.getMessage());
-//            throw new RuntimeException(timeOutError);
-//        }
-//        catch (SQLException accessError){
-//            LOG.error(accessError.getMessage());
-//            throw new RuntimeException(accessError);
-//        }
-//        finally
-//        {
-//            try {
-//                db.close();
-//            } catch (SQLException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
+        Utils.msgTitle("TP1 : Se connecter à une base de données locale");
 
-        Utils.msgConsign("TP3: Opérations CRUD");
+        final String URL = "jdbc:mysql://localhost:3306/DecouvrirJDBC_Compta";
+        final String USER = "root";
+        final String PWD = "root";
+
+        Utils.msgInfo("Connection à la base de données");
+        try(Connection db1 = DriverManager.getConnection(URL,USER,PWD))
+        {
+            Utils.msgInfo("Connection à la base de données OK");
+        } catch (SQLException e) {
+            LOG.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        Utils.msgTitle("TP2 : Créer un fichier de configuration pour l'accès aux données");
+
+        Utils.msgInfo("Import des propriétés de connection dans le fichiers properties");
+        ResourceBundle project = ResourceBundle.getBundle("project");
+        String URL_BY_PROP = project.getString("database.url");
+        String USER_BY_PROP = project.getString("database.user");
+        String PWD_BY_PROP = project.getString("database.pwd");
+        Utils.msgInfo("Import des ressources OK");
+
+        Utils.msgInfo("Connection à la base de données distante (Clever-cloud)");
+
+        //On utilise ici le bloc finally
+        try
+        {
+            db = DriverManager.getConnection(
+                    URL_BY_PROP,
+                    USER_BY_PROP,
+                    PWD_BY_PROP);
+            Utils.msgInfo("Connection à la base de données OK");
+        }
+        catch (SQLException accessError){
+            LOG.error(accessError.getMessage());
+            throw new RuntimeException(accessError);
+        }
+        finally
+        {
+            try {
+                db.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Utils.msgTitle("TP3: Opérations CRUD");
         Fournisseur.insert("La maison de la peinture");
         Fournisseur.updateName("La maison de la peinture", "La maison des peintures");
         Fournisseur.delete("La maison des peintures");
@@ -86,7 +81,5 @@ public class Main {
     }
 
 
-    public static int addition(int a, int b) {
-        return a + b;
-    }
+
 }
--- src/main/java/fr/diginamic/database/Database.java
+++ src/main/java/fr/diginamic/database/Database.java
@@ -17,18 +17,12 @@ public class Database {
     public static Connection connect() {
         Logger log = LoggerFactory.getLogger(Main.class);
         Connection db = null;
-        String msg;
 
-        msg = String.format("Connection à la base de données %s", URL);
-        Utils.msgInfo(msg);
+        Utils.msgInfo(String.format("Connection à la base de données %s", URL));
 
         try {
             db = DriverManager.getConnection(URL, USER, PWD);
-            msg = String.format("Connection à la base de données %s OK", URL);
-            Utils.msgInfo(msg);
-        } catch (SQLTimeoutException timeOutError) {
-            log.error(timeOutError.getMessage());
-            throw new RuntimeException(timeOutError);
+            Utils.msgInfo(String.format("Connection à la base de données %s OK", URL));
         } catch (SQLException accessError) {
             log.error(accessError.getMessage());
                 throw new RuntimeException();
@@ -45,7 +39,7 @@ public class Database {
                 Utils.beep();
                 log.warn("Tentative de fermeture d'une base de données non référencée en mémoire");
                 Utils.msgWarning("Tentative de fermeture d'une base de données non référencée en mémoire");
-                db = null;
+                return null;
             }
             db.close();
         } catch (SQLException e) {
@@ -58,94 +52,77 @@ public class Database {
     }
 
 
-    public static int insertValue(Connection db, String table, String column, String value) {
-        Statement st = null;
-        String msg = null;
-        int nbreLines = 0;
-
-        msg = String.format("Enregistrement de la valeur %s dans la table %s(%s)", value, table, column);
-        Utils.msgInfo(msg);
-
-        try {
-            st = db.createStatement();
-            msg = String.format("INSERT INTO %s(%s) VALUES('%s')", table, column, value);
-            nbreLines = st.executeUpdate(msg);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                st.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        msg = String.format("Insertion dans la table OK. Nbre de lignes insérées : %d", nbreLines);
-        Utils.msgResult(msg);
-        return nbreLines;
-    }
-
-    public static int listTableRows(Connection db, String table, String column) {
-        Statement st = null;
-        String msg = null;
-        int nbreLines = 0;
-        ResultSet rs = null;
-
-        msg = String.format("Lecture des valeurs de la colonne %s dans la table %s", column, table);
-        Utils.msgInfo(msg);
-        try {
-            st = db.createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            msg = String.format("SELECT * FROM %s;", table);
-            Utils.msgConsign(msg);
-            rs = st.executeQuery(msg);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + "-" + rs.getString(column));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            rs.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            st.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return nbreLines;
-    }
-
-    public static int updateValue(Connection db, String table, String column, String old_value, String newValue) {
-        String req = "UPDATE %s SET %s = '%s' WHERE %s = '%s'";
-        String msg = null;
-        Statement st = null;
-        int nb = 0;
-        try {
-            st = db.createStatement();
-            msg = String.format(req, table, column, newValue, column, old_value);
-            Utils.msgConsign(msg);
-            nb = st.executeUpdate(msg);
-            msg = String.format("Nbre de lignes traitées: %d", nb);
-            Utils.msgConsign(msg);
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                st.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return nb;
-    }
+//    public static int insertValue(Connection db, String table, String column, String value) {
+//        Statement st = null;
+//        String msg = null;
+//        int nbreLines = 0;
+//
+//        msg = String.format("Enregistrement de la valeur %s dans la table %s(%s)", value, table, column);
+//        Utils.msgInfo(msg);
+//
+//        try {
+//            st = db.createStatement();
+//            msg = String.format("INSERT INTO %s(%s) VALUES('%s')", table, column, value);
+//            nbreLines = st.executeUpdate(msg);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            try {
+//                st.close();
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        msg = String.format("Insertion dans la table OK. Nbre de lignes insérées : %d", nbreLines);
+//        Utils.msgResult(msg);
+//        return nbreLines;
+//    }
+//
+//    public static int listTableRows(Connection db, String table, String column) {
+//        Statement st = null;
+//        String msg = null;
+//        int nbreLines = 0;
+//        ResultSet rs = null;
+//        Utils.msgInfo(String.format("Lecture des valeurs de la colonne %s dans la table %s", column, table));
+//
+//        try {
+//            st = db.createStatement();
+//            msg = String.format("SELECT * FROM %s;", table);
+//            Utils.msgConsign(msg);
+//            rs = st.executeQuery(msg);
+//            while (rs.next()) {
+//                System.out.println(rs.getInt(1) + "-" + rs.getString(column));
+//            }
+//            rs.close();
+//            st.close();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return nbreLines;
+//    }
+//
+//    public static int updateValue(Connection db, String table, String column, String old_value, String newValue) {
+//        String req = "UPDATE %s SET %s = '%s' WHERE %s = '%s'";
+//        String msg = null;
+//        Statement st = null;
+//        int nb = 0;
+//        try {
+//            st = db.createStatement();
+//            msg = String.format(req, table, column, newValue, column, old_value);
+//            Utils.msgConsign(msg);
+//            nb = st.executeUpdate(msg);
+//            msg = String.format("Nbre de lignes traitées: %d", nb);
+//            Utils.msgConsign(msg);
+//        } catch (SQLException e) {
+//            System.err.println(e.getMessage());
+//        } finally {
+//            try {
+//                st.close();
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        return nb;
+//    }
 }
